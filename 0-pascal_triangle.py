a598426f01e19a53caeb65542df07e84a6dae59c
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-02 16:15:21 +0300

    pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that generates the Pascal's triangle"""
+
+def pascal_triangle(n):
+    triangle = []
+
+    if n <= 0:
+        return []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
+    return triangle
