0ab3f39448b3e42cee604ee46415f27d55ca4995
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-15 14:17:14 +0300

    handled extremely large numbers and corrected pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewest number of
+operations needed to result in exactly n H characters in the file.
 Prototype: def minOperations(n)
 Returns an integer
 If n is impossible to achieve, return 0
@@ -7,17 +10,18 @@ Example:
 
 n = 9
 
-H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste =>
+HHHHHH => Paste => HHHHHHHHH
 
 Number of operations: 6
 """
 
+
 def minOperations(n):
     """finds the least possible number of operations to get n characters"""
     counter = 0
-    if n <= 1:
-        return 0
-    for i in range(2, n + 1):
+    if n <= 1 or n > 10**6:
+        return 0    for i in range(2, n + 1):
         while n % i == 0:
             counter += i
             n = n / i
